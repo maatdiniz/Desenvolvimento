cb1250cef255a362c3ac03b8d220f8aea1a1e040
Matheus Diniz <diniz_matt@outlook.com>
2024-10-07 10:53:19 -0300

    Adicao Login

--- web/Login/main.js
+++
@@ -1,4 +0,0 @@
-// Ao clicar no botão de logout, redireciona o usuário para a página de login
-document.getElementById('logoutButton').addEventListener('click', function () {
-    window.location.href = "index.html"; // Redireciona para a página de login
-});
